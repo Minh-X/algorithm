142b364b9e4c0781226100e04c92971b58396e24
poon <746618739@qq.com>
2023-01-11 20:10:15 +0800

    牛客HJ1

---
+++ src/main/java/algorithm7/niuke/HJ1.java
@@ -0,0 +1,36 @@
+package algorithm7.niuke;
+
+import java.util.Scanner;
+
+/**
+ * @Author: permission
+ * @Date: 2023/1/11 18:41
+ * @Version: 1.0
+ * @ClassName: HJ1
+ * @Description: 牛客 hj1
+ */
+public class HJ1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        c2(str);
+    }
+
+    static void c2(String str) {
+        // 正则表达式实用性更强(str.split("\\s+"))
+        String[] s = str.split(" ");
+        System.out.println(s[s.length - 1].length());
+    }
+
+    static void c1(String str) {
+        int total = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (' ' != (str.charAt(i))) {
+                total++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(total);
+    }
+}
