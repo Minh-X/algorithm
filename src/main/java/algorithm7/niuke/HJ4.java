67b016c1fdf891b7284cd51d3b7601ee081f2d5c
poon <746618739@qq.com>
2023-01-11 21:47:28 +0800

    牛客HJ4

---
+++ src/main/java/algorithm7/niuke/HJ4.java
@@ -0,0 +1,40 @@
+package algorithm7.niuke;
+
+import java.util.Scanner;
+
+/**
+ * @Author: permission
+ * @Date: 2023/1/11 21:23
+ * @Version: 1.0
+ * @ClassName: HJ4
+ * @Description: 字符串分隔
+ */
+public class HJ4 {
+
+    private static final int SPLITNUM = 8;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            if (!str.isEmpty()) {
+                mySplit(str);
+            }
+        }
+    }
+
+    private static void mySplit(String str) {
+        while (str.length() > SPLITNUM) {
+            String s = str.substring(0, SPLITNUM);
+            System.out.println(s);
+            str = str.substring(SPLITNUM);
+        }
+        if (str.length() <= SPLITNUM && str.length() > 0) {
+            str = str + "00000000";
+            String s = str.substring(0, 8);
+            System.out.println(s);
+        }
+    }
+
+
+}
