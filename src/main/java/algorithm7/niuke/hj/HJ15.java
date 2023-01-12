16503cd70d7b87f7d5f6930f0a1a26aa1eb86f49
poon <746618739@qq.com>
2023-01-12 15:44:47 +0800

    HJ15

---
+++ src/main/java/algorithm7/niuke/hj/HJ15.java
@@ -0,0 +1,8 @@
+package algorithm7.niuke.hj;/**
+ * @Author: permission         
+ * @Date: 2023/1/12 15:13
+ * @Version: 1.0
+ * @ClassName: HJ15
+ * @Description: 求int型正整数在内存中存储时1的个数
+ */public class HJ15 {
+}
