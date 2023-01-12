ae138751a94a26922f1b8f2ca4662a9d743d1bdf
poon <746618739@qq.com>
2023-01-12 14:53:00 +0800

    牛客HJ13

---
+++ src/main/java/algorithm7/niuke/hj/HJ13.java
@@ -0,0 +1,27 @@
+package algorithm7.niuke.hj;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * @Author: permission
+ * @Date: 2023/1/12 14:42
+ * @Version: 1.0
+ * @ClassName: HJ13
+ * @Description: 句子逆序
+ */
+public class HJ13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        c1(str);
+    }
+
+    private static void c1(String str) {
+        String[] strArr = str.split(" ");
+        for (int i = strArr.length - 1; i >= 0 ; i--) {
+            System.out.print(strArr[i] + " ");
+        }
+    }
+}
