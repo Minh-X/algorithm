779ae99576b12d6d2ac0654bb7a3808b75edf548
poon <746618739@qq.com>
2023-02-04 17:16:01 +0800

    LC55

--- src/main/java/algorithm7/leetcode/贪心/LC55.java
+++ src/main/java/algorithm7/leetcode/贪心/LC55.java
@@ -8,4 +8,48 @@ package algorithm7.leetcode.贪心;
  * @Description: 55. 跳跃游戏
  */
 public class LC55 {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+//        int[] nums = {3, 2, 1, 0, 4};
+//        int[] nums = {0, 2, 3};
+        boolean res = canJump(nums);
+        System.out.println(res);
+    }
+
+
+    public static boolean canJump(int[] nums) {
+//        return handle1(nums);
+        return handle_LC(nums);
+    }
+
+    private static boolean handle1(int[] nums) {
+        int n = nums.length;
+        int canMaxIndex = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (i <= canMaxIndex) {
+                canMaxIndex = Math.max(canMaxIndex, i + nums[i]);
+            }
+        }
+        if (canMaxIndex < nums.length - 1) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private static boolean handle_LC(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
