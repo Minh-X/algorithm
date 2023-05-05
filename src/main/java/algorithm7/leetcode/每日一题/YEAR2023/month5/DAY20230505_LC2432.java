348737a594b070da9f9d9c5a7b6e290526a38e7d
merrick <746618739@qq.com>
2023-05-05 14:29:46 +0800

    init

---
+++ src/main/java/algorithm7/leetcode/每日一题/YEAR2023/month5/DAY20230505_LC2432.java
@@ -0,0 +1,14 @@
+package algorithm7.leetcode.每日一题.YEAR2023.month5;
+
+public class DAY20230505_LC2432 {
+
+    public int hardestWorker(int n, int[][] logs) {
+
+        return handle1(n, logs);
+    }
+
+    private int handle1(int n, int[][] logs) {
+        return 0;
+    }
+
+}
